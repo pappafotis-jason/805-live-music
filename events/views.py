483af3c5fe1d40a1ddf49ac9395d5ebd04dc8645
Jason Pappafotis <jason@ratlabrecords.com>
2025-07-20 22:56:55 +0000

    Fix add_event view with proper redirect import

--- events/views.py
+++ events/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
-=======
 from django.shortcuts import render, redirect
 from django.views.decorators.cache import cache_page
 from .models import Event
@@ -13,13 +9,16 @@ def add_event(request):
         if form.is_valid():
             form.save()
             return redirect('map_view')
+        else:
+            print("Form errors:", form.errors)  # Optional debug
     else:
         form = EventForm()
-    return render(request, 'add_event.html', {'form': form})
+    return render(request, 'events/add_event.html', {'form': form})
 
-@cache_page(60 * 15)  # Cache for 15 minutes using Redis
+@cache_page(60 * 15)  # Cache for 15 minutes
 def map_view(request):
-    events = Event.objects.all()
-    return render(request, 'map.html', {'events': events})
+    events = Event.objects.all()  # Fetch all events
+    print("Events queried:", events)  # Debug output
+    return render(request, 'events/map.html', {'events': events})
 
 # Expand with calendar view later
