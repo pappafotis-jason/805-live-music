2eb831c7ac76c1d267c3e824fb653e0d329f08d7
Jason Pappafotis <jason@ratlabrecords.com>
2025-07-20 23:30:15 +0000

    Reduce map view cache to 1 minute for faster event updates

--- events/views.py
+++ events/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.decorators.cache import cache_page
 from .models import Event
 from .forms import EventForm
+from datetime import datetime
 
 def add_event(request):
     if request.method == 'POST':
@@ -10,15 +11,23 @@ def add_event(request):
             form.save()
             return redirect('map_view')
         else:
-            print("Form errors:", form.errors)  # Optional debug
+            print("Form errors:", form.errors)
     else:
         form = EventForm()
     return render(request, 'events/add_event.html', {'form': form})
 
-@cache_page(60 * 15)  # Cache for 15 minutes
+@cache_page(60 * 1)
 def map_view(request):
-    events = Event.objects.all()  # Fetch all events
-    print("Events queried:", events)  # Debug output
+    date_str = request.GET.get('date')
+    if date_str:
+        try:
+            date = datetime.strptime(date_str, '%Y-%m-%d').date()
+            events = Event.objects.filter(date_time__date=date)
+        except ValueError:
+            events = Event.objects.all()
+    else:
+        events = Event.objects.all()
+    print("Map view rendered with events:", list(events))  # Debug output
     return render(request, 'events/map.html', {'events': events})
 
 # Expand with calendar view later
