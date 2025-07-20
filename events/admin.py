b4173571ad4fdd25f0d7e9f36cadcc19d4e4bd69
Jason Pappafotis <jason@ratlabrecords.com>
2025-07-20 08:44:15 +0000

    Save local changes to models.py and views.py before merging remote

---
+++ events/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here if needed, e.g.:
+# from .models import Event, Venue, Act
+# admin.site.register(Event)
+# admin.site.register(Venue)
+# admin.site.register(Act)
