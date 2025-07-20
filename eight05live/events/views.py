0ea144cca0e6c0516313504db6ac1678c123953f
Jason Pappafotis <jason@ratlabrecords.com>
2025-07-20 02:56:42 -0500

    Create views.py

---
+++ eight05live/events/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.views.decorators.cache import cache_page
+from .models import Event
+from .forms import EventForm
+
+def add_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('map_view')
+    else:
+        form = EventForm()
+    return render(request, 'add_event.html', {'form': form})
+
+@cache_page(60 * 15)  # Cache for 15 minutes using Redis
+def map_view(request):
+    events = Event.objects.all()
+    return render(request, 'map.html', {'events': events})
+
+# Expand with calendar view later
